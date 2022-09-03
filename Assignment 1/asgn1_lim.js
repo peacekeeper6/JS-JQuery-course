8c2b6d95023771a555dad14a70df57fefe95e83b
peacekeeper6 <junlim954@gmail.com>
2022-09-03 15:34:41 -0700

    assignment 1

--- Assignment 1/asgn1_lim.js
+++ Assignment 1/asgn1_lim.js
@@ -6,12 +6,20 @@ var displayMessage = function ()
 {
 	var myFirstName = $("firstname").value;
 	var myAge = $("age").value;
+	var hours = $("hours").value;
 	
 	var myAgeInt = parseInt(myAge);
+	var hoursInt = parseInt(hours);
 	
-	var myTrueAge = myAgeInt + 10;
+	var num = myAgeInt * (hoursInt/24);
 	
-	var myText = "Hi there, " + myFirstName + "you are not " + myAge + " but actually " + myTrueAge
+	// Can also do it this way
+	// var num = Math.round(myAgeInt * (hoursInt/24))
+
+	var roundedNum = Math.round(num);
+	
+	var myText = "Hi " + myFirstName + ". You have slept " + roundedNum + " years of your life away."
+	
 	$("mymsg").innerHTML = myText;
 }
 
@@ -19,3 +27,4 @@ window.onload = function ()
 {
 	$("mybutton").onclick = displayMessage;
 }
+
